cdfdd086c151b4750b93f2541ac11632b4cbfc77
mroneup <glabusjakub@gmail.com>
2024-02-12 02:35:47 +0100

    Update EmployeeAssembly.php
    Add a few additional columns

--- Modules/HR/Migration/EmployeeAssembly.php
+++ Modules/HR/Migration/EmployeeAssembly.php
@@ -10,7 +10,7 @@ final class Employee
     public function up(): string
     {
         // this up() migration is auto-generated, please modify it to your needs
-        return 'CREATE TABLE employees (id INT AUTO_INCREMENT NOT NULL, identityid VARCHAR(36) NOT NULL, first_name VARCHAR(255) NOT NULL, second_name VARCHAR(255) NOT NULL, address LONGTEXT NOT NULL, position LONGTEXT NOT NULL, employed TINYINT(1) NOT NULL, hired DATE NOT NULL, fired DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`';
+        return 'CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, identityid VARCHAR(36) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, birth_date DATE NOT NULL, address LONGTEXT NOT NULL, salary INT NOT NULL, position LONGTEXT NOT NULL, employed TINYINT(1) NOT NULL, hired DATE NOT NULL, fired DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`';
     }
 
     public function down(): string
